693756bdacfc6751d615f254725b0177e1724dc9
sachinjacob40 <sachinjacob40@gmail.com>
2021-07-23 12:02:03 +0530

    Added loader

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './About.scss'
 const About = ({
   collapsed,
   rtl,
@@ -11,15 +11,52 @@ const About = ({
   collapse
 }) => {
   
+
+  const[loading,setLoading]=React.useState(0);
+
+
+  React.useEffect(()=>{
+    let k=0;
+    const l=setInterval(()=>{
+     k=k+5;
+     if(k<=100){
+      setLoading(k)
+     }
+     
+    },1000)
+
+    return ()=>{
+      clearInterval(l);
+    }
+  },[])
   
   return (
     <main style={{marginLeft:collapse?50:250}}>
-      <div className="btn-toggle" onClick={() => handleToggleSidebar(true)}>
-      </div>
-      <header>
-        <p>About Page</p>
-        <p>description</p>
-      </header>  
+      
+    <div style={{background: '#d0d0d0',margin:'0rem 21rem'}}>
+        <section>
+            <article>
+                <div class="chart">
+                    <div class="bar bar-75 white">
+                        <div class="face top">
+                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                        </div>
+                        <div class="face side-0">
+                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                        </div>
+                        <div class="face floor">
+                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                        </div>
+                        <div class="face side-a"></div>
+                        <div class="face side-b"></div>
+                        <div class="face side-1">
+                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                        </div>
+                    </div>
+                </div>
+                </article>
+                </section>
+                </div>
     </main>
   );
 };
