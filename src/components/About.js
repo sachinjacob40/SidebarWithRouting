862031489d8fe4b0ab485ab771369ced5131e30d
sachinjacob40 <sachinjacob40@gmail.com>
2021-07-24 22:43:18 +0530

    Code improvements

--- src/components/About.js
+++ src/components/About.js
@@ -32,25 +32,25 @@ const About = ({
   
   return (
     <main style={{marginLeft:collapse?50:250}}>
-      
+    
     <div style={{background: '#d0d0d0',margin:'0rem 21rem'}}>
         <section>
             <article>
                 <div class="chart">
                     <div class="bar bar-75 white">
                         <div class="face top">
-                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                            <div class="growing-bar" style={{width:`${loading}%`}}></div>
                         </div>
                         <div class="face side-0">
-                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                            <div class="growing-bar" style={{width:`${loading}%`}}></div>
                         </div>
                         <div class="face floor">
-                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                            <div class="growing-bar" style={{width:`${loading}%`}}></div>
                         </div>
                         <div class="face side-a"></div>
                         <div class="face side-b"></div>
                         <div class="face side-1">
-                            <div class="growing-bar" style={{width:`${loading}%`,backgroundColor:'orange'}}></div>
+                            <div class="growing-bar" style={{width:`${loading}%`}}></div>
                         </div>
                     </div>
                 </div>
